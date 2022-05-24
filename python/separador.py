819364e7dc262973d32904c15d5610eb2d2abeca
Luis <lantonium@outlook.com>
2022-05-24 18:57:26 -0500

    Create reto2

---
+++ python/separador.py
@@ -0,0 +1,21 @@
+def separador(cadena):
+    aux = cadena.split(" ")
+    nombre = aux[0]
+    apellido = aux[1]
+    return nombre, apellido
+
+resultado = separador('Luis Gonzalez')
+print(type(resultado), resultado)
+
+def separador(cadena):
+    aux = cadena.split(" ")
+    nombre = aux[0]
+    apellido = aux[1]
+    return nombre, apellido
+
+resultado = separador('Luis Gonzalez')
+print(type(resultado), resultado)
+
+
+
+    
